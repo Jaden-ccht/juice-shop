09ad9d73f7989fd3554718c7503a5c4941f42510
Jaden COUCHOT <jaden.c43@gmail.com>
2024-02-09 16:32:02 +0100

    fix: Arbitrary file read

--- routes/fileServer.ts
+++ routes/fileServer.ts
@@ -24,17 +24,25 @@ module.exports = function servePublicFiles () {
   }
 
   function verify (file: string, res: Response, next: NextFunction) {
-    if (file && (endsWithAllowlistedFileType(file) || (file === 'incident-support.kdbx'))) {
-      file = security.cutOffPoisonNullByte(file)
-
-      challengeUtils.solveIf(challenges.directoryListingChallenge, () => { return file.toLowerCase() === 'acquisitions.md' })
-      verifySuccessfulPoisonNullByteExploit(file)
-
-      res.sendFile(path.resolve('ftp/', file))
+    file = file.replace(/[\0]+/g, '')
+    if (isFileExtensionValid(file)) {
+      const filePath = path.resolve('ftp/', file)
+      if (filePath.startsWith(path.resolve('ftp/'))) {
+        res.sendFile(filePath)
+      } else {
+        res.status(403).send('Forbidden.')
+      }
     } else {
-      res.status(403)
-      next(new Error('Only .md and .pdf files are allowed!'))
+      res.status(403).send('Invalid file type.')
     }
+    challengeUtils.solveIf(challenges.directoryListingChallenge, () => { return file.toLowerCase() === 'acquisitions.md' })
+    verifySuccessfulPoisonNullByteExploit(file)
+  }
+
+  function isFileExtensionValid (file: string) {
+    const allowedExtensions = ['.md', '.pdf']
+    const extension = path.extname(file).toLowerCase()
+    return allowedExtensions.includes(extension)
   }
 
   function verifySuccessfulPoisonNullByteExploit (file: string) {
